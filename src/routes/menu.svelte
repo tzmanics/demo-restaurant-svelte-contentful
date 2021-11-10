ee0553042ee85316f1803da80522c89ed2453141
tzmanics <tzmanics@gmail.com>
2021-11-09 22:06:18 -0500

    adds menu page

--- src/routes/menu.svelte
+++ src/routes/menu.svelte
@@ -1,1 +1,15 @@
-<h1>menu</h1>
+<script>
+	import MenuItem from '../components/MenuItem.svelte';
+	import menu from '../data/menu.json';
+	import page from '../data/pages.json';
+</script>
+
+<main>
+	<h1 class="section-title">{page.menu.title}</h1>
+	<div class="section-intro">
+		{page.menu.description}
+	</div>
+	{#each menu as item}
+		<MenuItem {item} />
+	{/each}
+</main>
